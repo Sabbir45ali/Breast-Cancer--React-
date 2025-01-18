063f908f5332911aacec4be662b285ef1a992c86
TitlyC06 <prathamac62@gmail.com>
2025-01-18 17:38:07 +0530

    RightButtonAdded

--- src/Components/LogIn page/RightModel/Input.jsx
+++ src/Components/LogIn page/RightModel/Input.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
+
 const Input = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center">
       <form className="w-80 space-y-4">
         <input
           type="text"
@@ -25,6 +26,7 @@ const Input = () => {
           className="w-full p-3 rounded-lg bg-gray-200 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400"
         />
       </form>
+    
     </div>
   );
 };
