d7a90f159ce7735bd1d232570c6b5d636f5be55a
Sayanipal2004 <rinapal619@gmail.com>
2025-01-18 22:03:50 +0530

    RightHeader added

--- src/Components/LogIn page/RightModel/MainRightModel.jsx
+++ src/Components/LogIn page/RightModel/MainRightModel.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Input from "./Input";
 import RightButton from "./RightButton";
+import RightHeader from "./RightHeader";
 const MainRightModel = () => {
   return (
     <div className="flex flex-col gap-y-4 items-center justify-center ">
+      <RightHeader/>
       <Input/>
       <div className="">
       <RightButton/>
--- src/Components/LogIn page/RightModel/RightButton.jsx
+++ src/Components/LogIn page/RightModel/RightButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const RightButton = () =>{
     return(
         
-       <button className="w-36 bg-pink-200 text-pink-950 text-lg py-2 px-6 rounded-lg hover:bg-pink-600 transition duration-300">
+       <button className="w-36 bg-pink-500 text-white text-lg py-2 px-6 shadow-lg rounded-lg hover:bg-pink-200 hover:text-pink-950 transition duration-300">
             SIGN-IN
           </button>
     )
