13409a260048c7642b44a63cfb1f8bc655afcc36
TitlyC06 <prathamac62@gmail.com>
2025-01-22 00:36:20 +0530

    signup page form done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/LoginPage/Login";
+import Signup from "./pages/signuppage/Signup";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
---
+++ src/Components/Signup page Component/signupinput.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import InputField from "../LogIn page Component/LoginPageRightModel/InputField"; 
+
+const Input = () => {
+
+  const inputFields = [
+
+    { type: "email", placeholder: "Email" },
+    { type: "password", placeholder: "Password" },
+  ];
+
+  return (
+    <div className="flex items-center justify-center p-14">
+      <form className="w-80 space-y-4">
+        {inputFields.map((field, index) => (
+          <InputField
+            key={index}
+            type={field.type}
+            placeholder={field.placeholder}
+          />
+        ))}
+      </form>
+    </div>
+  );
+};
+
+
+export default Input;
+
+
