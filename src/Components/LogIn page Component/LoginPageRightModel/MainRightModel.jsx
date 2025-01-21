1201babb103847f10306653fc5497b69d39a0ded
Sayanipal2004 <rinapal619@gmail.com>
2025-01-21 22:39:57 +0530

    props added in Header and MainRightModel

--- src/Components/LogIn page Component/LoginPageRightModel/MainRightModel.jsx
+++ src/Components/LogIn page Component/LoginPageRightModel/MainRightModel.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Input from "./Input";
 import RightButton from "./RightButton";
-import RightHeader from "./RightHeader";
+import RightHeader from "./Header";
 const MainRightModel = () => {
   return (
-    <div className="flex flex-col gap-y-4 items-center justify-center ">
-      <RightHeader/>
+    <div className="flex flex-col gap-y-4 items-center justify-center">
+      <RightHeader FirstLetter='C' SecondLetter='A' Firstpart='reate ' Secondpart='ccount'/>
       <Input/>
       <div className="">
-      <RightButton/>
+      <RightButton btn='SIGN-UP'/>
       </div>
     </div>
   )
