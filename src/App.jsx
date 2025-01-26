d38db71529683e4129276ccfe472e134f8d671b5
Sayanipal2004 <rinapal619@gmail.com>
2025-01-26 15:22:42 +0530

    landingPage responsiveness done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React , {useState,useEffect} from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/LoginPage/Login";
 import LandingPage from "./pages/LandingPage/LandingPage";
-import Signup from "./pages/signuppage/Signup";
+// import Signup from "./pages/signuppage/Signup";
 import Mobile_landingPage from "./pages/Mobile_LandingPage/Mobile_landingPage";
+const ResponsivePage = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return isMobile ? <Mobile_landingPage /> : <LandingPage />;
+};
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/LandingPage" element={<LandingPage />} />
-        <Route path="/landing_M" element={<Mobile_landingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<ResponsivePage />} />
+        {/* <Route path="/LandingPage" element={<LandingPage />} />
+        <Route path="/landing_M" element={<Mobile_landingPage />} /> */}
       </Routes>
     </Router>
   );
