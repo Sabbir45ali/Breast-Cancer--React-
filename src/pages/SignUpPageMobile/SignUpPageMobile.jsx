9b8e148a97dfe1115ce369dfc9ce50b7e455bd6a
TitlyC06 <prathamac62@gmail.com>
2025-02-11 21:14:51 +0530

    BgSignUp done

--- src/pages/SignUpPageMobile/SignUpPageMobile.jsx
+++ src/pages/SignUpPageMobile/SignUpPageMobile.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import SignInPageInputes from '../../Components/SignInPage_Mobile_Components/SignInPageInput'
-import SignInPage_Buttons from '../../Components/SignInPage_Mobile_Components/SignInPage_Button'
-import Header from '../../Components/LogIn page Component/LoginPageRightModel/Header'
 import BGImage from '../../assets/Images/SignInPageMobileBg.png'
-
+import SignupButton from '../../Components/Signup page Component/SignupButton'
+import Header from '../../Components/LogIn page Component/LoginPageRightModel/Header'
 const SignUpPageMobile = () => {
   const inputFields = [
     { type: 'text', placeholder: 'Enter your name', background: '#F3DCE0' },
@@ -24,21 +23,29 @@ const SignUpPageMobile = () => {
     <div className=''>
       <div
         style={{
-          backgroundImage: `url(${BgImageSignup})`,
+          backgroundImage: `url(${BGImage})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center'
         }}
         className='min-h-screen flex flex-col items-center justify-center  px-4 sm:px-6'
       >
-        <div className='w-full max-w-xs sm:max-w-sm md:max-w-md bg-white p-6 rounded-2xl shadow-lg absolute bottom-4 right-4 '>
+        <div className='w-full max-w-xs sm:max-w-sm md:max-w-md bg-white p-6 rounded-2xl shadow-lg absolute bottom-0 right-4 '>
+          <Header
+          FirstLetter='C'
+          Firstpart='reate'
+          SecondLetter='A'
+          Secondpart='ccount'
+          />
           <SignInPageInputes inputs={inputFields} />
 
-          <SigninPage_Buttons
+          <div className='flex items-center justify-center  '>
+          <SignupButton
             label='Sign-Up'
             bgColor='#FF6699'
             textColor='#FFFFFF'
-            className='w-full mt-4'
+            className='w-full mt-4 '
           />
+          </div>
         </div>
       </div>
     </div>
