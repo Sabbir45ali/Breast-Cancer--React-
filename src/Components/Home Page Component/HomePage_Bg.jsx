0941259a301c5c84fc279cf9206ae3f2c2ca0e0a
Sayanipal2004 <rinapal619@gmail.com>
2025-02-15 01:23:42 +0530

    HomePage BG added

---
+++ src/Components/Home Page Component/HomePage_Bg.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const HomePage_Bg=()=>{
+    return (
+        <>
+        <div className="bg-[url('/src/assets/Images/bg_pic_landing_page_dekstop.png')] bg-cover bg-center h-screen flex flex-col overflow-auto items-center justify-center">
+          
+          </div>
+        </>
+      );
+}
+export default HomePage_Bg;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,16 @@
 // skipcq: JS-W1028
 import React from 'react'
 import Navbar from '../../Components/Universal Components/Navbar'
+import HomePage_Bg from '../../Components/Home Page Component/HomePage_Bg'
 // skipcq: JS-C1001, JS-0128
 import profileImg from '../../assets/Images/profileIcon.png'
 import Logo from '../../assets/Images/logo.png'
 const HomePage = () => {
   return (
     <div className=''>
+      
       <Navbar profileImg={profileImg} Logo={Logo} />
+      <HomePage_Bg/>
     </div>
   )
 }
