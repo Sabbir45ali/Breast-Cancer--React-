a1d399c2fa721b2a71636532ec9bd5ffd10f31c7
TitlyC06 <prathamac62@gmail.com>
2025-02-14 23:51:50 +0530

    ProfilePageBgAdded

---
+++ src/pages/ProfilePage/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    
+      <div className="bg-[url('src/assets/Images/ProfilePage.png')] bg-cover bg-center h-screen ">
+  
+</div>
+    
+  )
+}
+
+export default Profile
