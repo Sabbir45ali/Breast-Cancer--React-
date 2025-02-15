f58cc968748437c025969fc22669abe51d4846f6
Sabbir45ali <sabbir84b@gmail.com>
2025-02-15 21:25:47 +0530

    feature add footer component to mobile homepage

--- src/pages/HomePage_Mobile/HomePage_Mobile.jsx
+++ src/pages/HomePage_Mobile/HomePage_Mobile.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import NavbarMobile from "../../Components/HomePageMobileComponents/NavbarMobile";
+import FooterMobile from "../../Components/HomePageMobileComponents/FooterMobile";
 
 const HomePage_Mobile = () => {
   return (
-    <NavbarMobile/>
+    <div className="flex flex-col min-h-screen">
+      <NavbarMobile />
+      
+      {/* Main Content (Add your page content here) */}
+      <div className="flex-grow"></div>
+      
+      <FooterMobile />
+    </div>
   );
 };
 
