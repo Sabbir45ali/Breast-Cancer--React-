98112bd669dbdd1a79b9f245b8fded18b792e482
TitlyC06 <prathamac62@gmail.com>
2025-02-15 23:13:39 +0530

    Updated

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <div className="bg-[url('src/assets/Images/ProfilePage.png')] bg-cover bg-center h-screen " />
+    <div className="bg-[url('src/assets/Images/ProfilePage.png')] bg-cover bg-center h-screen " ></div>
   );
 };
 
