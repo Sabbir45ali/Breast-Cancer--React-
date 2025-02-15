6ceb227045de0f63c32b335b4297af7342f1b73c
Sabbir45ali <sabbir84b@gmail.com>
2025-02-15 22:48:54 +0530

    feature add Header component to HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,16 @@
 // skipcq: JS-W1028
-import React from 'react'
-import Navbar from '../../Components/Universal Components/Navbar'
+import React from "react";
+import Navbar from "../../Components/Universal Components/Navbar";
 // skipcq: JS-C1001, JS-0128
-import profileImg from '../../assets/Images/profileIcon.png'
-import Logo from '../../assets/Images/logo.png'
+import profileImg from "../../assets/Images/profileIcon.png";
+import Logo from "../../assets/Images/logo.png";
+import Header from "../../Components/Home Page Components/Header";
 const HomePage = () => {
   return (
-    <div className=''>
+    <div className="">
+      <Header />
       <Navbar profileImg={profileImg} Logo={Logo} />
     </div>
-  )
-}
-export default HomePage
+  );
+};
+export default HomePage;
