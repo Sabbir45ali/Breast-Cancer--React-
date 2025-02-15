0fa28fd2040ecd6319db6fb06669887c4a739ce6
Sabbir45ali <sabbir84b@gmail.com>
2025-02-15 20:57:22 +0530

    feature add mobile homepage and navbar components

--- src/pages/HomePage_Mobile/HomePage_Mobile.jsx
+++ src/pages/HomePage_Mobile/HomePage_Mobile.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import NavbarMobile from "../../Components/HomePageMobileComponents/NavbarMobile";
 
 const HomePage_Mobile = () => {
   return (
-    <div>
-      
-    </div>
+    <NavbarMobile/>
   );
-}
+};
+
 export default HomePage_Mobile;
