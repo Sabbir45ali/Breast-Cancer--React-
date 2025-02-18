8a2f3b605323174dbd1cdf7479f20422c74edb8e
TitlyC06 <prathamac62@gmail.com>
2025-02-18 23:12:48 +0530

    Updated Position

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
   return (
     <div>
       <div class="bg-[url('src/assets/Images/desktop_landingPage.png')] bg-cover bg-center h-screen">
-      <div className='text-center relative top-12'>
+      <div className='text-center relative top-20'>
         <h1 className='text-5xl font-bold text-black mb-2 mr-52 '>Welcome to</h1>
         <h2
           className='text-4xl font-bold  mt-2 ml-44'
@@ -22,7 +22,7 @@ const LandingPage = () => {
         <img
           src={doctorimage}
           alt='Centered Image'
-          className="block mx-auto w-74 h-74 relative top-16"
+          className="block mx-auto w-74 h-74 relative top-28"
  
         />
       </div>
