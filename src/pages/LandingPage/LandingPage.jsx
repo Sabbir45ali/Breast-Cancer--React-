734a350c39b95fa00766659ab930d1bf3cea508c
TitlyC06 <prathamac62@gmail.com>
2025-02-19 23:26:05 +0530

    Updated

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,38 +1,31 @@
 import React from 'react'
-import doctorimage from '../../assets/Images/MobileLandingPageFemale1.png'
+import doctorimage from '../../assets/Images/DoctorImage.png'
 
 import { TbArrowBigRightLinesFilled } from 'react-icons/tb'
 import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
-    <div>
-      <div class="bg-[url('src/assets/Images/desktop_landingPage.png')] bg-cover bg-center h-screen">
+    <div className="bg-[url('src/assets/Images/desktop_landingPage.png')] bg-cover bg-center h-screen overflow-hidden">
       <div className='text-center relative top-20'>
         <h1 className='text-5xl font-bold text-black mb-2 mr-52 '>Welcome to</h1>
-        <h2
-          className='text-4xl font-bold  mt-2 ml-44 text-[#890A0C]'
-          
-        >
+        <h2 className='text-4xl font-bold mt-2 ml-44 text-[#890A0C]'>
           Care Memmo
         </h2>
       </div>
-      <div>
+      <div className='w-74 h-74'>
         <img
           src={doctorimage}
           alt='Centered Image'
           className="block mx-auto w-74 h-74 relative top-28"
- 
         />
       </div>
-    
 
-        <Link to='/signin'>
-          <div className=' absolute bottom-4 right-4 '>
-            <TbArrowBigRightLinesFilled className='text-3xl text-black' />
-          </div>
-        </Link>
-      </div>
+      <Link to='/signin'>
+        <div className='absolute bottom-4 right-4'>
+          <TbArrowBigRightLinesFilled className='text-3xl text-black' />
+        </div>
+      </Link>
     </div>
   )
 }
