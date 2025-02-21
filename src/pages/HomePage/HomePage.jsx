c90aa30a832dad3339075959cdce4f92407d5b07
Sabbir45ali <sabbir84b@gmail.com>
2025-02-21 15:15:25 +0530

    fix: correct logo image filename casing in HomePage component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Navbar from '../../Components/Universal Components/Navbar'
 // skipcq: JS-C1001, JS-0128
 import profileImg from '../../assets/Images/profileIcon.png'
-import Logo from '../../assets/Images/logo.png'
+import Logo from '../../assets/Images/Logo.png'
 import Header from '../../Components/Home Page Components/Header'
 const HomePage = () => {
   return (
