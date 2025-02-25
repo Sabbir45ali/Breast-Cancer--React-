a9541f0f93a2c58bed639426dcf7d339e5c18618
Sayanipal2004 <rinapal619@gmail.com>
2025-02-25 22:28:02 +0530

    Bg Image added in HomePage[responsive version]

--- src/pages/HomePage_Mobile/HomePage_Mobile.jsx
+++ src/pages/HomePage_Mobile/HomePage_Mobile.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import NavbarMobile from '../../Components/HomePageMobileComponents/NavbarMobile'
 import FooterMobile from '../../Components/HomePageMobileComponents/FooterMobile'
+import bg_pic_Home_page_mobile from '../../assets/Images/bg_pic_Home_page_mobile.png'
+ 
+
 
 const HomePage_Mobile = () => {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div 
+    style={{ backgroundImage: `url(${bg_pic_Home_page_mobile})` }}
+    className='flex flex-col min-h-screen bg-cover bg-center'>
       <NavbarMobile />
       <div className='flex-grow' />
       <FooterMobile />
