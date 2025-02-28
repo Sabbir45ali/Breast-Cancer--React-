da7d81b57c8cf6bbeefdafb181f2d4ff542d80cd
Sabbir45ali <sabbir84b@gmail.com>
2025-02-28 15:38:43 +0530

    feat: add HomePageHeader component to Mobile homepage

--- src/pages/HomePage_Mobile/HomePage_Mobile.jsx
+++ src/pages/HomePage_Mobile/HomePage_Mobile.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import NavbarMobile from '../../Components/HomePageMobileComponents/NavbarMobile'
-import FooterMobile from '../../Components/HomePageMobileComponents/FooterMobile'
+import React from "react";
+import NavbarMobile from "../../Components/HomePageMobileComponents/NavbarMobile";
+import FooterMobile from "../../Components/HomePageMobileComponents/FooterMobile";
 // import bg_pic_Home_page_mobile from '../../assets/Images/bg_pic_Home_page_mobile.png'
-
+import HomePageHeader from "../../Components/HomePageMobileComponents/HomaPageHeader";
 const HomePage_Mobile = () => {
   return (
     <div className="bg-[url('/src/assets/Images/bg_pic_Home_page_mobile.png')] flex flex-col min-h-screen bg-cover bg-center">
       <NavbarMobile />
-      <div className='flex-grow' />
+      <HomePageHeader />
+      <div className="flex-grow"></div>
       <FooterMobile />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage_Mobile
+export default HomePage_Mobile;
