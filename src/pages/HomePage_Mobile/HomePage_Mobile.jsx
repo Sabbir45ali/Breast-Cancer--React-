0d4eb8120ce3a322ba84e1e2d3a0bf5b16ccae97
TitlyC06 <prathamac62@gmail.com>
2025-03-01 22:56:45 +0530

    HalfDone

--- src/pages/HomePage_Mobile/HomePage_Mobile.jsx
+++ src/pages/HomePage_Mobile/HomePage_Mobile.jsx
@@ -3,12 +3,17 @@ import NavbarMobile from '../../Components/HomePageMobileComponents/NavbarMobile
 import FooterMobile from '../../Components/HomePageMobileComponents/FooterMobile'
 // import bg_pic_Home_page_mobile from '../../assets/Images/bg_pic_Home_page_mobile.png'
 import HomePageHeader from '../../Components/HomePageMobileComponents/HomaPageHeader'
+import AwarenessMobile from '../../Components/HomePageMobileComponents/AwarenessMobile'
 const HomePage_Mobile = () => {
   return (
-    <div className="bg-[url('/src/assets/Images/bg_pic_Home_page_mobile.png')] flex flex-col min-h-screen bg-cover bg-center">
+    <div>
+    <div className="bg-[url('/src/assets/Images/bg_pic_Home_page_mobile.png')] flex flex-col min-h-screen bg-cover bg-center overflow-auto">
       <NavbarMobile />
       <HomePageHeader />
-      <div className='flex-grow' />
+    
+    </div>
+    <AwarenessMobile />
+    <div className='flex-grow' />
       <FooterMobile />
     </div>
   )
