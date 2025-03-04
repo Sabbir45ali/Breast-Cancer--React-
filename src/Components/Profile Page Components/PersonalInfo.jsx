baec80e8bb003394caa452780f13518cff482a4b
Sayanipal2004 <rinapal619@gmail.com>
2025-03-05 00:19:58 +0530

    React fragment removed from the ProfileInfo.jsx page

--- src/Components/Profile Page Components/PersonalInfo.jsx
+++ src/Components/Profile Page Components/PersonalInfo.jsx
@@ -4,37 +4,40 @@ import { FaPhone } from 'react-icons/fa6'
 
 const PersonalInfo = (props) => {
   return (
-    <>
-      <div className='bg-black w-32 h-32 absolute -top-24 rounded-full z-10 overflow-hidden'>
+    <div className="relative flex flex-col items-center w-full">
+     
+      <div className="bg-black w-32 h-32 absolute -top-16 rounded-full z-10 overflow-hidden">
         <img
-          src='/src/assets/Images/profileIcon.png'
-          alt='profilePicture'
-          className='w-32 h-32 object-cover'
+          src="/src/assets/Images/profileIcon.png"
+          alt="profilePicture"
+          className="w-32 h-32 object-cover"
         />
       </div>
-      <div className=' py-12 px-40 text-center flex flex-col items-center gap-0 absolute -top-4'>
-        <div className='flex flex-col justify-center items-center relative'>
-          <h1 className='text-2xl font-bold gap-2'>{props.name}</h1>
-          <div className='flex flex-row gap-1'>
-            <MdEmail className='absolute top-10 left-0 text-gray-900 text-xl' />
-            <p className='text-gray-900 text-lg font-semibold'>{props.email}</p>
-          </div>
+
+    
+      <div className="mt-16 text-center flex flex-col items-center">
+        <h1 className="text-2xl font-bold">{props.name}</h1>
+        <div className="flex items-center gap-2 mt-1">
+          <MdEmail className="text-gray-900 text-xl" />
+          <p className="text-gray-900 text-lg font-semibold">{props.email}</p>
         </div>
-        <div className='flex justify-center items-center text-center gap-10 mt-4 text-gray-800 text-[15px] font-semibold'>
-          <div className='flex flex-row gap-2'>
-            <FaPhone className='text-xl' />
+
+       
+        <div className="flex justify-center items-center gap-10 mt-4 text-gray-800 text-[15px] font-semibold">
+          <div className="flex items-center gap-2">
+            <FaPhone className="text-xl" />
             <span>{props.phnNo}</span>
           </div>
-          <div className='flex flex-row gap-2'>
-            <span className='font-bold'>Age-{props.age}</span>
+          <div className="flex items-center gap-2">
+            <span className="font-bold">Age: {props.age}</span>
           </div>
-          <div className='flex flex-row gap-2'>
-            <MdBloodtype className='text-2xl' />
-            <span> Group-{props.bloodGroup}</span>
+          <div className="flex items-center gap-2">
+            <MdBloodtype className="text-2xl" />
+            <span>Group: {props.bloodGroup}</span>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
