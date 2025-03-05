eeb634040fd7b4f78388dd4ea15b11a1df2a5acb
Sayanipal2004 <rinapal619@gmail.com>
2025-03-05 22:00:05 +0530

    Font grid issue fixed in the header of home page[Desktop version]

--- src/Components/Home Page Components/Header.jsx
+++ src/Components/Home Page Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='text-right relative mt-24 py-24 px-8 w-full flex'>
-      <div className='w-[80%] max-w-6xl ml-auto'>
+      <div className='w-[80%] max-w-6xl ml-80 px-10'>
         <h1 className='text-[4.25rem] leading-[4.5rem] font-extrabold text-white text-right'>
           Strength is not in facing cancer, but in fighting it with hope and
           resilience.
