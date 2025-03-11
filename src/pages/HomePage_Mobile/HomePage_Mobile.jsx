971fe7ef7b044ae675d7ae6257cd55bfa7a3d071
Sabbir45ali <sabbir84b@gmail.com>
2025-03-11 23:33:18 +0530

    Add background styling for mobile homepage and remove unused footer import

--- src/pages/HomePage_Mobile/HomePage_Mobile.jsx
+++ src/pages/HomePage_Mobile/HomePage_Mobile.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import NavbarMobile from '../../Components/HomePageMobileComponents/NavbarMobile'
-import FooterMobile from '../../Components/HomePageMobileComponents/FooterMobile'
+// import FooterMobile from '../../Components/HomePageMobileComponents/FooterMobile'
 import HomePageHeader from '../../Components/HomePageMobileComponents/HomaPageHeader'
 import Awareness from '../../Components/HomePageMobileComponents/AwarenessMobile'
+import '../../App.css'
 
 const HomePage_Mobile = () => {
   return (
     <div className='flex flex-col gap-0'>
       <div
-        className='flex flex-col min-h-screen bg-cover bg-center overflow-auto'
-        style={{
-          backgroundImage: "url('/assets/Images/bg_pic_Home_page_mobile.png')",
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
-        }}
+        className='HomeBg flex flex-col min-h-screen bg-cover bg-center overflow-auto'
+        // style={{
+        //   backgroundImage: "url('/assets/Images/bg_pic_Home_page_mobile.png')",
+        //   backgroundSize: 'cover',
+        //   backgroundPosition: 'center'
+        // }}
       >
         <NavbarMobile />
         <HomePageHeader />
