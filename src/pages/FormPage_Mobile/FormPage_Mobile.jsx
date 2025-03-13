f0e6171ccc8e1ee6d8d898d27cf45b31f9ba94e4
PayalRoy2004 <payal2004@gmail.com>
2025-03-13 22:58:06 +0530

    update

--- src/pages/FormPage_Mobile/FormPage_Mobile.jsx
+++ src/pages/FormPage_Mobile/FormPage_Mobile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FormPage_Mobile() {
   return (
-    <div className="bg-[url('src/assets/Images/bg_pic_Form_Page_mobile.png')] bg-cover bg-center min-h-screen flex flex-col overflow-auto">
+    <div className="bg-[url('src/assets/Images/bg_pic_Form_Page_mobile.png')] bg-cover bg-no-repeat bg-center min-h-screen flex flex-col overflow-auto">
       
     </div>
   )
