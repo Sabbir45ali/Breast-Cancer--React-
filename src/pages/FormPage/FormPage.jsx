b4e9d3add013ad5e0e58defcf88c00c54903c2fd
PayalRoy2004 <payal2004@gmail.com>
2025-03-13 22:15:26 +0530

    Add bg in form page [Desktop version].

--- src/pages/FormPage/FormPage.jsx
+++ src/pages/FormPage/FormPage.jsx
@@ -5,7 +5,7 @@ import profileImg from '../../assets/Images/profileIcon.png'
 
 const FormPage = () => {
   return (
-    <div>
+    <div className="bg-[url('src/assets/Images/bg_pic_Form_page_dekstop.png')] bg-cover bg-center min-h-screen flex flex-col overflow-auto" >
       <Navbar Logo={Logo} profileImg={profileImg} />
     </div>
   )
