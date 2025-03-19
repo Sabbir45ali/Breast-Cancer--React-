41d6ecdb1d2547062c01fef22436d306273381ba
Sayanipal2004 <rinapal619@gmail.com>
2025-03-19 22:27:21 +0530

    SignUp page[responsive version] completed

--- src/Components/SignInPage_Mobile_Components/SignInPage_Footer.jsx
+++ src/Components/SignInPage_Mobile_Components/SignInPage_Footer.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { BsArrowUpSquareFill } from 'react-icons/bs'
-const FooterLinks = (props) => {
+import { Link } from 'react-router-dom'
+
+const FooterLinks = ({ FooterText1, FooterText2, link }) => {
   return (
-    <div className='flex flex-col items-center w-full'>
-      <div className='  rounded-2xl w-full bg-gradient-to-r from-pink-300 to-pink-500 text-white text-center py-4 rounded-t-2xl font-semibold mb-0'>
-        {`${props.FooterText1}`}
-        <a href='/sign-up' className='font-bold'>
-          {props.FooterText2}{' '}
-          <BsArrowUpSquareFill className='relative bottom-5 left-80 text-xl ' />
-        </a>
+    <div className='flex items-center w-full'>
+      <div className='rounded-2xl w-full bg-gradient-to-r from-pink-300 to-pink-500 text-white text-center py-4 rounded-t-2xl font-semibold mb-0'>
+        
+        {/* Use Link for navigation */}
+        <Link to={link} className='text-white no-underline hover:text-gray-200'>
+          <span>{FooterText1}</span>
+          <span className='font-bold ml-2'>
+            {FooterText2}{' '}
+            <BsArrowUpSquareFill className='inline-block text-xl ml-1' />
+          </span>
+        </Link>
       </div>
     </div>
   )
 }
 
 export default FooterLinks
+
 
 // I've already done this story , I just raised this PR
