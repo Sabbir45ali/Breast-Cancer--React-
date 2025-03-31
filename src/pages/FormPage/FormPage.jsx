6c7f637f6fdd0246222206ea9b36677cec61cd66
PayalRoy2004 <payal2004@gmail.com>
2025-03-31 23:52:17 +0530

    Add bg in form page [Desktop version].

--- src/pages/FormPage/FormPage.jsx
+++ src/pages/FormPage/FormPage.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Navbar from '../../Components/Universal Components/Navbar'
 import Logo from '../../assets/Images/Logo.png'
 import profileImg from '../../assets/Images/profileIcon.png'
+import '../../App.css'
 
 const FormPage = () => {
   return (
-    <div>
+    <div className='formPageDekstop min-h-screen w-full' >
       <Navbar Logo={Logo} profileImg={profileImg} />
     </div>
   )
