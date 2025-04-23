4ee38b961aeb4dd9668f9801dbecffa2386095a6
PayalRoy2004 <payal2004@gmail.com>
2025-04-23 21:59:19 +0530

    Navbar Added (Mobile Version)

--- src/pages/FormPage_Mobile/FormPage_Mobile.jsx
+++ src/pages/FormPage_Mobile/FormPage_Mobile.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "../../App.css";
+import NavbarMobile from "../../Components/Form Page Components/FormPageNavbar"
 function FormPage_Mobile() {
   return (
-    <div className="form_bg min-h-screen flex flex-col overflow-auto"> </div>
+    <div className="form_bg min-h-screen flex flex-col overflow-auto">
+      <div>
+      <NavbarMobile />
+      </div>
+     </div>
   );
 }
 
