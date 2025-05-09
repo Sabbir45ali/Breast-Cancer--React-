c66f50f3bea0e24b3c175021869381cda239ab5b
Sayanipal2004 <rinapal619@gmail.com>
2025-05-09 22:18:15 +0530

    Form page for single customer[Mobile version] is created and navbar is added

---
+++ src/pages/FormPageBtoC_Mobile/FormPageBtoC_Mobile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../App.css";
+import NavbarMobile from "../../Components/Form Page Components/FormPageNavbar";
+
+const FormPageBtoC_Mobile = () => {
+  return (
+    <div className='formBtoC_mob'>
+        <NavbarMobile/>
+    </div>
+  )
+}
+
+export default FormPageBtoC_Mobile
