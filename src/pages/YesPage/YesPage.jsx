7cbc7fc8df40f2e54fb5ff77d302580b022680b3
TitlyC06 <prathamac62@gmail.com>
2025-05-10 23:02:47 +0530

    YessPageDone

--- src/pages/YesPage/YesPage.jsx
+++ src/pages/YesPage/YesPage.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Navbar from "../../Components/Universal Components/Navbar";
 import profileImg from "../../assets/Images/profileIcon.png";
 import Logo from "../../assets/Images/Logo.png";
+import TextBox from "../../Components/YesPage/TextBox"
 const YesPage = () => {
   return (
     <div>
       <div className=" bg-cover bg-center min-h-screen flex flex-col overflow-auto bg_Yespage">
         <Navbar profileImg={profileImg} Logo={Logo} />
+         <div>
+        <TextBox/>
+         </div>
       </div>
+      
     </div>
   );
 };
