5acb554386d99454ee63745f140261fb6425fe9f
PayalRoy2004 <payal2004@gmail.com>
2025-05-14 00:34:30 +0530

    Added text on form page of Single customer

--- src/pages/NoPage/NoPage.jsx
+++ src/pages/NoPage/NoPage.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "../../Components/Universal Components/Navbar";
 import profileImg from "../../assets/Images/profileIcon.png";
 import Logo from "../../assets/Images/Logo.png";
+import TextBox from "../../Components/NoPage/TextBox";
 
 const NoPage = () => {
   return (
     <div>
       <div className="NoPageBG bg-cover bg-center min-h-screen flex flex-col overflow-auto">
         <Navbar profileImg={profileImg} Logo={Logo} />
+        <div>
+          <TextBox/>
+        </div>
       </div>
     </div>
   );
