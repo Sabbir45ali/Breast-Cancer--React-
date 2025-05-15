4acbd60a06de3a6e3c99e64a041eb2035bfb4ce7
Sabbir45ali <sabbir84b@gmail.com>
2025-05-15 23:18:38 +0530

    feat: add TextBoxBtoC component to FormPageBtoC_Mobile

--- src/pages/FormPageBtoC_Mobile/FormPageBtoC_Mobile.jsx
+++ src/pages/FormPageBtoC_Mobile/FormPageBtoC_Mobile.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../../App.css";
 import NavbarMobile from "../../Components/Form Page Components/FormPageNavbar";
-
+import TextBoxBtoC from "../../Components/Form Page BtoC Components/TextBoxBtoC";
 const FormPageBtoC_Mobile = () => {
   return (
     <div className=" min-h-screen formBtoC_mob">
       <NavbarMobile />
+      <TextBoxBtoC />
     </div>
   );
 };
