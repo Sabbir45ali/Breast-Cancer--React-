2fa895c328a08c08d8d33bfb7f08fced24027e01
Sabbir45ali <sabbir84b@gmail.com>
2025-06-01 12:00:41 +0530

    fix: update base path in Vite config for development and production environments

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/breast-cancer-detection'
+  base: mode === 'development' ? '' : '/Breast-Cancer--React-/'
 }))
