010789d38096bde4d296da95b9f5dc214be23047
Sayanipal2004 <rinapal619@gmail.com>
2025-06-26 14:41:18 +0530

    UI mapped

--- src/Components/SignIn page Component/SignInButton.jsx
+++ src/Components/SignIn page Component/SignInButton.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const SignInButton = (props) => {
+const SignInButton = ({ btnColor, role }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    switch (role) {
+      case 'User':
+        navigate('/home');
+        break;
+      case 'Admin':
+        navigate('/admin-home');
+        break;
+      case 'Organisation':
+        navigate('/org-home');
+        break;
+      default:
+        alert('Please select a role.');
+    }
+  };
+
   return (
-    <Link to='/home'>
-      <button
-        style={{ backgroundColor: props.btnColor }}
-        className='w-36 text-white text-lg py-2 px-6 bg-[#AB1B68] shadow-lg rounded-lg hover:bg-pink-200 hover:text-pink-950 transition duration-300'
-      >
-        SIGN-IN
-      </button>
-    </Link>
-  )
-}
+    <button
+      onClick={handleClick}
+      style={{ backgroundColor: btnColor }}
+      className='w-36 text-white text-lg py-2 px-6 bg-[#AB1B68] shadow-lg rounded-lg hover:bg-pink-200 hover:text-pink-950 transition duration-300'
+    >
+      SIGN-IN
+    </button>
+  );
+};
 
-export default SignInButton
+export default SignInButton;
