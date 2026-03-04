870ca50215e189a3623794924a366f1111f42158
Sabbir45ali <sabbir84b@gmail.com>
2026-03-04 12:36:40 +0530

    fix

--- src/pages/SignInPage_Mobile/SignINPage_Mobile.jsx
+++ src/pages/SignInPage_Mobile/SignINPage_Mobile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SignInPage_bg from '../../Components/SignInPage_Mobile_Components/SignInPage_bg'
-//for deploy
+
 const SignInPage_Mobile = () => {
   return (
     <SignInPage_bg />
