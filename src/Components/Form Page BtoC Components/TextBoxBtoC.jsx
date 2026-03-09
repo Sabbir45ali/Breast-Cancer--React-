7f3de1a286145c5cfd13b1545e1d880df6800db8
Sabbir45ali <sabbir84b@gmail.com>
2026-03-09 17:34:02 +0530

    feat: implement mammogram image upload page with file type/size validation and prediction API integration.

--- src/Components/Form Page BtoC Components/TextBoxBtoC.jsx
+++ src/Components/Form Page BtoC Components/TextBoxBtoC.jsx
@@ -89,12 +89,6 @@ export default function MammogramUploadPage() {
 
   return (
     <div className='min-h-screen flex flex-col items-center justify-center relative overflow-hidden'>
-      <img
-        src='/background-flower.png'
-        alt='Background Flower'
-        className='absolute inset-0 w-full h-full object-cover opacity-60 -z-10'
-      />
-
       <div className='text-center px-6 '>
         <h1 className='text-4xl md:text-5xl font-semibold text-black/80 leading-relaxed mb-10 gap-y-10'>
           Early <br /> Detection <br /> Saves <br /> Lives
